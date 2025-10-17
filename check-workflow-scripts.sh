17d69c15715ebbd3e91c16eff1c43c83f949b98b
Paul Colby <git@colby.id.au>
2025-10-17 21:03:23 +1100

    Export the extra variables (if any)

--- check-workflow-scripts.sh
+++ check-workflow-scripts.sh
@@ -246,6 +246,8 @@ function checkWorkflow {
           jq -r '.env//{}|keys[]|"export "+.' <<< "${job}"
           echo '# Step environment variables'
           jq -r '.env//{}|keys[]|"export "+.' <<< "${step}"
+          echo '# Extra variables'
+          printf 'export %s=\n' "${extraVars[@]}"
           # shellcheck disable=SC2016 # The GitHub Actions expression is not meant to me expanded.
           echo '# Shell script (with ${{ ... }} expressions removed)'
           jq -r '.run' <<< "${step}" | sed -e 's|\${{[^}]\+}}||g'
